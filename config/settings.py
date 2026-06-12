285c80fafc06f50204075f90c06da09ff56189f6
codewithsovi <sovialfina@gmail.com>
2026-06-12 21:30:02 +0700

    models

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'categories'
+    'categories',
+    'books',
+    'peminjam'
 ]
 
 MIDDLEWARE = [
